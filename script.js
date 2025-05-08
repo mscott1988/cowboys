e84a59b553578a9714af9f6b085e5109f8994e0c
mscott1988 <michael@cowboysac.com>
2025-05-08 17:01:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,62 +14,61 @@ const units = {
           "ahri": "215691194",
           "total_price": 4284,
           "tax_credit": true
-        }
-      }
-    },
-    "N5A5S": {
-      "label": "N5A5S Single Stage AC",
-      "options": {
-        "2 Ton": {
-          "price": 1385,
-          "coil": "EAM5X24M14A",
-          "coil_price": 445,
-          "furnace": "N80MSN0451412A",
-          "furnace_price": 617,
-          "seer2": 16.0,
-          "eer2": 13.0,
-          "ahri": "214913620",
-          "total_price": 2447,
-          "tax_credit": false
-        }
-      }
-    }
-  },
-  "Century HVAC Supplies": {
-    "YC6": {
-      "label": "YC6 2-Stage AC",
-      "options": {
-        "2 Ton": {
-          "price": 1703.84,
-          "coil": "CTM24A5ABS1",
-          "coil_price": 528.83,
-          "furnace": "Y81E060A12SMPS1",
-          "furnace_price": 846.57,
-          "seer2": 17.25,
-          "eer2": 12.75,
-          "ahri": "216600741",
-          "total_price": 3079.24,
+        },
+        "3 Ton": {
+          "price": 2983,
+          "coil": "EAM5X36M17A",
+          "coil_price": 475,
+          "furnace": "G80CTL0902120B",
+          "furnace_price": 1580,
+          "seer2": 18.0,
+          "eer2": 11.0,
+          "ahri": "215696475",
+          "total_price": 5038,
+          "tax_credit": true
+        },
+        "4 Ton": {
+          "price": 3271,
+          "coil": "EAM5X48M21A",
+          "coil_price": 500,
+          "furnace": "G80CTL0902120B",
+          "furnace_price": 1580,
+          "seer2": 18.5,
+          "eer2": 10.5,
+          "ahri": "215700584",
+          "total_price": 5351,
           "tax_credit": true
         }
       }
     },
-    "YH4": {
-      "label": "YH4 Split Heat Pump",
+    "C5A8T": {
+      "label": "C5A8T Ion Two-Stage AC",
       "options": {
         "2 Ton": {
-          "price": 1780.38,
-          "coil": "JHE24B5AC2SS1",
-          "coil_price": 1095.28,
-          "furnace": "S1-8HK06500506",
-          "furnace_price": 63.42,
-          "seer2": 14.3,
-          "eer2": 11.0,
-          "ahri": "214424692",
-          "total_price": 2939.08,
-          "tax_credit": false
+          "price": 2051,
+          "coil": "EVM5X24M14A",
+          "coil_price": 343,
+          "furnace": "N80VSL0451412B",
+          "furnace_price": 674,
+          "seer2": 16.5,
+          "eer2": 13.0,
+          "ahri": "216377101",
+          "total_price": 3068,
+          "tax_credit": true
+        },
+        "3 Ton": {
+          "price": 2228,
+          "coil": "EAM5X36M17A",
+          "coil_price": 564,
+          "furnace": "N80VSL0701716B",
+          "furnace_price": 699,
+          "seer2": 15.5,
+          "eer2": 12.5,
+          "ahri": "216393919",
+          "total_price": 3491,
+          "tax_credit": true
         }
       }
     }
   }
 };
-
