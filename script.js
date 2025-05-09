e7671c56e6df8bcb448297aace0cf327c7d8561f
mscott1988 <michael@cowboysac.com>
2025-05-08 21:13:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,91 @@
-const units = {}; // Replace with real data
+const units = {
+  "Robert Madden": {
+    "C5A1V": {
+      "label": "C5A1V Variable Speed AC",
+      "options": {
+        "2 Ton": {
+          "price": 2379,
+          "coil": "EAM5X24M14A",
+          "coil_price": 445,
+          "furnace": "G80CTL0701412B",
+          "furnace_price": 1460,
+          "seer2": 17.0,
+          "eer2": 10.5,
+          "ahri": "215691194",
+          "total_price": 4284,
+          "tax_credit": true
+        },
+        "3 Ton": {
+          "price": 2983,
+          "coil": "EAM5X36M17A",
+          "coil_price": 475,
+          "furnace": "G80CTL0902120B",
+          "furnace_price": 1580,
+          "seer2": 18.0,
+          "eer2": 11.0,
+          "ahri": "215696475",
+          "total_price": 5038,
+          "tax_credit": true
+        }
+      }
+    },
+    "C5A8T": {
+      "label": "C5A8T Ion Two-Stage AC",
+      "options": {
+        "2 Ton": {
+          "price": 2051,
+          "coil": "EVM5X24M14A",
+          "coil_price": 343,
+          "furnace": "N80VSL0451412B",
+          "furnace_price": 674,
+          "seer2": 16.5,
+          "eer2": 13.0,
+          "ahri": "216377101",
+          "total_price": 3068,
+          "tax_credit": true
+        },
+        "3 Ton": {
+          "price": 2228,
+          "coil": "EAM5X36M17A",
+          "coil_price": 564,
+          "furnace": "N80VSL0701716B",
+          "furnace_price": 699,
+          "seer2": 15.5,
+          "eer2": 12.5,
+          "ahri": "216393919",
+          "total_price": 3491,
+          "tax_credit": true
+        }
+      }
+    },
+    "N5A5S": {
+      "label": "N5A5S Single-Stage AC",
+      "options": {
+        "2 Ton": {
+          "price": 1375,
+          "coil": "EAM4X24L17A",
+          "coil_price": 328,
+          "furnace": "N80ESN0451412A",
+          "furnace_price": 599,
+          "seer2": 14.3,
+          "eer2": 11.7,
+          "ahri": "213829151",
+          "total_price": 2302,
+          "tax_credit": false
+        },
+        "3 Ton": {
+          "price": 1480,
+          "coil": "EAM4X36L17A",
+          "coil_price": 352,
+          "furnace": "N80ESN0701716A",
+          "furnace_price": 620,
+          "seer2": 14.5,
+          "eer2": 12.0,
+          "ahri": "213829155",
+          "total_price": 2452,
+          "tax_credit": false
+        }
+      }
+    }
+  }
+};
