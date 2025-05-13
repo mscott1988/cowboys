c2a65a2ed461ed73b5a0b1d3f9892ebe67ca6e34
mscott1988 <michael@cowboysac.com>
2025-05-13 10:50:50 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+
+const data = {
+  "manufacturer": "Robert Madden",
+  "categories": ["Split Gas Electric Systems", "Split Heat Pump Systems", "Furnaces", "Air Handlers", "Evaporator Coils", "Heat Kits", "Air Conditioners", "Heat Pumps"]
+};
